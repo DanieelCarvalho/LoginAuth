0497567249a19d358b75f6e43dc360d8f11cee5b
daniel <daniescg2012@gmail.com>
2025-06-16 17:09:02 -0300

    add doker-compose

--- src/test/java/br/com/api/loginAuth/LoginAuthApplicationTests.java
+++ src/test/java/br/com/api/loginAuth/LoginAuthApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.api.cadastro;
+package br.com.api.loginAuth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CadastroApplicationTests {
+class LoginAuthApplicationTests {
 
 	@Test
 	void contextLoads() {
